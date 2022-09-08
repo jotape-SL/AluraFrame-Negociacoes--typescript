cbd7d9a1c1ee583b4d0540fb0bcf7ea4ad7e3bd8
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-09-08 11:41:04 -0300

    template string console render

---
+++ dist/views/negocicoes-view.js
@@ -0,0 +1,16 @@
+export default class NegociacoesView {
+    template() {
+        return `
+            <table class='table table-hover table-bordered'
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        `;
+    }
+}
