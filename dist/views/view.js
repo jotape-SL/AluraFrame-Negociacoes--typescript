f789dd4c49e15e682e7a24ccb46118e60d08b00e
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-09-11 15:45:13 -0300

    using a regex to prevent malicious scripts

--- dist/views/view.js
+++ dist/views/view.js
@@ -1,9 +1,18 @@
 export class View {
-    constructor(selector) {
+    // paramentos opcionais nao podem ser atrbuidos nos primeiros paramentros sendo necessario colocar-los por ultimo
+    constructor(selector, escapar) {
+        this.escapar = false;
         this.element = document.querySelector(selector);
+        if (escapar) {
+            this.escapar = escapar;
+        }
     }
     update(model) {
-        const template = this.template(model);
+        let template = this.template(model);
         this.element.innerHTML = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        this.element.innerHTML = template;
     }
 }
